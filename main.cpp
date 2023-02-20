eea98b3add2bc8d812afcaf065f7efa6bf80576c
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2023-02-20 18:28:28 +0200

    getAverage added

--- main.cpp
+++ main.cpp
@@ -13,10 +13,20 @@ int getSum(vector<int> &numbers)
     return total;
 }
 
+double getAverage(vector<int>& numbers){
+    double total = 0.0;
+    int sz = vector.size();
+    for(int& number : numbers)
+    {
+	total += number;
+    }
+    return (total/sz);
+}
 
 int main()
 {
     vector<int> numbers{6, 8, 2, 4, 6, 2, 28, 12, 17, 1};
     cout << getSum(numbers) << endl;
+    cout << getAverage(numbers) << endl;
     return 0;
 }
