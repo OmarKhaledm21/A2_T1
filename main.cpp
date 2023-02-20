85830bf485607249022b43a72ce452c55052f82d
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2023-02-20 18:11:51 +0200

    getSum function

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getSum(vector<int> &numbers)
+{
+    int total = 0;
+    for (int &number : numbers)
+    {
+        total += number;
+    }
+    return total;
+}
+
+int main()
+{
+    vector<int> numbers{6, 8, 2, 4, 6, 2, 28, 12, 17, 1};
+    cout << getSum(numbers) << endl;
+    return 0;
+}
