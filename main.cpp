b8777d3c89f31cce4e0067ec9b65866cf3b9ebfe
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2023-02-20 18:33:55 +0200

    getMin added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <limits.h>
 using namespace std;
 
 int getSum(vector<int> &numbers)
@@ -13,9 +13,19 @@ int getSum(vector<int> &numbers)
     return total;
 }
 
+int getMin(vector<int>& numbers)
+{
+    int min = INT_MAX;
+    for(int& number: numbers)
+    {
+      if(number<min) min = number;
+    }
+    return min;
+}
 int main()
 {
     vector<int> numbers{6, 8, 2, 4, 6, 2, 28, 12, 17, 1};
     cout << getSum(numbers) << endl;
+    cout << getMin(numbers) << endl;
     return 0;
 }
